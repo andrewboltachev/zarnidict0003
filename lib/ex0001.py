0a57f5d24ef70b9924e9e828316010d49682cdc4
Andrew Boltachev <andrewboltachev@gmail.com>
2015-01-08 22:36:26 +0300

    cool results

---
+++ lib/ex0001.py
@@ -0,0 +1,12 @@
+class SimpleBNFError(Exception):
+    pass
+
+
+def simple_bnf(grammar, data):
+    if isinstance(grammar['data'], dict):
+        return {'name': grammar['name'], 'data': simple_bnf(grammar['data'], data)}
+    else:
+        if data == grammar['data']:
+            return {'name': grammar['name'], 'data': data}
+        else:
+            raise SimpleBNFError('expected: "{0}", got: "{1}"'.format(grammar['data'], data))
---
+++ tests/ex0001.py
@@ -0,0 +1,62 @@
+import unittest
+from ..lib.ex0001 import simple_bnf, SimpleBNFError
+
+
+class SimpleBNFTestCase(unittest.TestCase):
+    def assertResult(self, grammar, data, result):
+        self.assertEqual(
+            simple_bnf(grammar, data),
+            result
+        )
+
+    def assertResultEqualToGrammar(self, grammar, data):
+        self.assertResult(grammar, data, grammar)
+
+    def assertResultIsBad(self, grammar, data):
+        with self.assertRaises(SimpleBNFError):
+            simple_bnf(grammar, data)
+
+    
+    def test_0001(self):
+        self.assertResultEqualToGrammar(
+            {'name': 'root', 'data': '.'},
+            '.'
+        )
+        self.assertResultEqualToGrammar(
+            {'name': 'root', 'data': ','},
+            ','
+        )
+
+    def test_0002(self):
+        self.assertResultIsBad(
+            {'name': 'root', 'data': ','},
+            '.'
+        )
+
+    # 2-level case
+    def test_0003(self):
+        self.assertResultEqualToGrammar(
+            {'name': 'root', 'data':
+                {'name': 'foo', 'data': ','}
+            },
+            ','
+        )
+
+    def test_0004(self):
+        self.assertResultIsBad(
+            {'name': 'root', 'data':
+                {'name': 'foo', 'data': ','}
+            },
+            '.'
+        )
+
+    # 3-level case
+    def t1est_0005(self):
+        self.assertResultEqualToGrammar(
+            {'name': 'root', 'data':
+                {'name': 'foo', 'data':
+                    {'name': 'bar', 'data': ','}
+                }
+            },
+            ','
+        )
