44feb5b96cec8198c7d2269e4208cf7f824006ba
Andrew Boltachev <andrewboltachev@gmail.com>
2015-01-08 22:53:07 +0300

    refactoring

--- lib/ex0001.py
+++ lib/ex0001.py
@@ -4,11 +4,10 @@ class SimpleBNFError(Exception):
 
 def simple_bnf(grammar, data):
     if isinstance(grammar['data'], dict):
-        return {'name': grammar['name'], 'data': simple_bnf(grammar['data'], data)}
+        data = simple_bnf(grammar['data'], data)
     elif isinstance(grammar['data'], list):
-        return {'name': grammar['name'], 'data': map(simple_bnf, grammar['data'], data)}
+        data = map(simple_bnf, grammar['data'], data)
     else:
-        if data == grammar['data']:
-            return {'name': grammar['name'], 'data': data}
-        else:
+        if data != grammar['data']:
             raise SimpleBNFError('expected: "{0}", got: "{1}"'.format(grammar['data'], data))
+    return {'name': grammar['name'], 'data': data}
